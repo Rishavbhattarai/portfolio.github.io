8e45e4d7313bc51b15d1587971191c0dfcb8591b
Rishav Bhattarai <017bim037@sxc.edu.np>
2024-06-17 11:43:31 +0545

    Calculator update

--- js/javascript.js
+++ js/javascript.js
@@ -16,3 +16,33 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+// CALCULATOR
+let expression = '';
+
+function appendNumber(number) {
+  expression += number;
+  document.getElementById('display').value = expression;
+}
+
+function appendOperator(operator) {
+  if (expression !== '') {
+    expression += operator;
+    document.getElementById('display').value = expression;
+  }
+}
+
+function clearDisplay() {
+  expression = '';
+  document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+  try {
+    const result = eval(expression);
+    document.getElementById('display').value = result;
+    expression = '';
+  } catch (error) {
+    document.getElementById('display').value = 'Error';
+  }
+}
