92544d74caedf62172f31781c8182f4cdfb7d734
Rishav Bhattarai <bhattr3@unlv.nevada.edu>
2026-02-19 02:05:01 -0800

    election changes in the website

---
+++ js/blog_script.js
@@ -0,0 +1,9 @@
+function openDashboard(dashboard) {
+  if (dashboard === 'one') {
+    window.location.href = '2082_candidates.html';
+  }
+
+  if (dashboard === 'two') {
+    window.location.href = 'Election_Commission.html';
+  }
+}
