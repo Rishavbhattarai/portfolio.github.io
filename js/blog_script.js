8897d8ded2c339b2d38371dd9c2520643c381bb1
Rishavbhattarai <bhattarai.rishav@gmail.com>
2026-08-24 14:37:30 -0700

    Removing Election Data

--- js/blog_script.js
+++ js/blog_script.js
@@ -1,12 +1,4 @@
 function openDashboard(dashboard) {
-  if (dashboard === 'one') {
-    window.location.href = '2082_candidates.html';
-  }
-
-  if (dashboard === 'two') {
-    window.location.href = 'Election_Commission.html';
-  }
-
   if (dashboard === 'three') {
     window.location.href = 'financial_chaos.html';
   }
